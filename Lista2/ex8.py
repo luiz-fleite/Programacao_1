afc264fe70d88c9c7269fa30b7b9d3c7ebe78d4d
Luiz1738 <102365054+Luiz1738@users.noreply.github.com>
2022-09-03 17:06:36 -0300

    atualização

--- Lista2/ex8.py
+++ Lista2/ex8.py
@@ -1,7 +1,12 @@
 
-data = str(input("Insira a data mo formato dd/mm/aaaa: "))
-dia = 0
-mes = 0
-ano = 0
+dia = int(input("Insira o dia: "))
+mes = int(input("Insira o mês: "))
+ano = int(input("Insira o ano: "))
 
+mes30 = [4, 6, 11]
 
+if (1 > mes > 12) or (1 > dia > 31) or (mes == 2 and dia > 29) or (dia > 30 and mes in mes30)\
+        or (ano % 4 != 0 and mes == 2 and dia == 29):
+    print("Data inválida.")
+else:
+    print("Data válida.")
